aa32bb8d13ff5691cf8930955b2a34010b8bb2bb
Manan <manankapiladps@gmail.com>
2022-07-02 10:56:16 +0530

    Hello World

---
+++ src/main/java/com/tgt/rysetii/learningsourcesapimanan_kapila/LearningsourcesApiMananKapilaApplication.java
@@ -0,0 +1,13 @@
+package com.tgt.rysetii.learningsourcesapimanan_kapila;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningsourcesApiMananKapilaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningsourcesApiMananKapilaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tgt/rysetii/learningsourcesapimanan_kapila/LearningsourcesApiMananKapilaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tgt.rysetii.learningsourcesapimanan_kapila;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningsourcesApiMananKapilaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
